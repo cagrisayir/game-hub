e55e00a994a8d482622d27ce5baf21fe3afdb7d2
Omer Cagri Sayir <omer.sayir@ogr.sakarya.edu.tr>
2023-10-06 12:03:50 +0300

    chakra theme changed

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
 	initialColorMode: 'dark',
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+	config,
+	colors: {
+		gray: {
+			50: '#E5E5E5',
+			100: '#DBDBDB',
+			200: '#C6C6C6',
+			300: '#B2B2B2',
+			400: '#9D9D9D',
+			500: '#898989',
+			600: '#6D6D6D',
+			700: '#515151',
+			800: '#353535',
+			900: '#111',
+		},
+	},
+});
 
 export default theme;
