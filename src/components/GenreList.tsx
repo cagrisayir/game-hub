1bc711c81044b6cca43a0bcee83e5cfa08c955b1
Omer Cagri Sayir <omer.sayir@ogr.sakarya.edu.tr>
2023-10-06 11:32:50 +0300

    clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,6 +6,7 @@ import {
 	Text,
 	Spinner,
 	Button,
+	Heading,
 } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
@@ -21,29 +22,37 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
 	if (error) return null;
 	if (isLoading) return <Spinner />;
 	return (
-		<List>
-			{data.map(genre => (
-				<ListItem key={genre.id} paddingY='5px'>
-					<HStack>
-						<Image
-							boxSize='32px'
-							borderRadius={8}
-							src={getCroppedImageUrl(genre.image_background)}
-						/>
-						<Button
-							fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-							onClick={() => {
-								onSelectGenre(genre);
-							}}
-							variant='link'
-							fontSize={'lg'}
-						>
-							{genre.name}
-						</Button>
-					</HStack>
-				</ListItem>
-			))}
-		</List>
+		<>
+			<Heading marginBottom={3} fontSize='2xl'>
+				Genres
+			</Heading>
+			<List>
+				{data.map(genre => (
+					<ListItem key={genre.id} paddingY='5px'>
+						<HStack>
+							<Image
+								boxSize='32px'
+								borderRadius={8}
+								objectFit='cover'
+								src={getCroppedImageUrl(genre.image_background)}
+							/>
+							<Button
+								whiteSpace='normal'
+								textAlign='left'
+								fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+								onClick={() => {
+									onSelectGenre(genre);
+								}}
+								variant='link'
+								fontSize={'lg'}
+							>
+								{genre.name}
+							</Button>
+						</HStack>
+					</ListItem>
+				))}
+			</List>
+		</>
 	);
 };
 
