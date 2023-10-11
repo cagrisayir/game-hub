c434e53e01c9ddb6c6c1f39a4e9b93f2f25e02a8
Omer Cagri Sayir <omer.sayir@ogr.sakarya.edu.tr>
2023-10-11 20:29:23 +0300

    fetch game details

--- src/types.ts
+++ src/types.ts
@@ -19,7 +19,9 @@ export interface Platform {
 export interface Game {
 	id: number;
 	name: string;
+	slug: string;
 	background_image: string;
+	description_raw: string;
 	parent_platforms: { platform: Platform }[];
 	metacritic: number;
 }
